62edc8c456864bc71f1e1948d84c891c3f58f6e0
Javier Rico <javi921@hotmail.es>
2017-06-10 23:22:33 +0100

    Added simple service test

---
+++ 11thLauncher.Tests/Services/ServerQueryServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using _11thLauncher.Model;
+using _11thLauncher.Model.Server;
+using _11thLauncher.Services;
+
+namespace _11thLauncher.Tests.Services
+{
+    [TestFixture, Category("Services")]
+    public class ServerQueryServiceTests
+    {
+        private IServerQueryService _serverQueryService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _serverQueryService = new ServerQueryService();
+        }
+
+        [Test]
+        public void CheckServerStatus_Online()
+        {
+            //Arrange
+            var server = new Server()
+            {
+                Name = "TestServer",
+                Address = "11thmeu.es",
+                Port = 2302,
+                IsDefault = true,
+                IsEnabled = true
+            };
+
+            //Act
+            _serverQueryService.CheckServerStatus(server);
+
+            //Assert
+            Assert.That(server.ServerStatus, Is.EqualTo(ServerStatus.Online));
+        }
+    }
+}
