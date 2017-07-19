c1c9710dc42a783c1eec30585499f5480447257a
Javier Rico <javi921@hotmail.es>
2017-07-19 23:06:05 +0100

    Reorder constants file

--- 11thLauncher/Constants.cs
+++ 11thLauncher/Constants.cs
@@ -9,97 +9,12 @@ namespace _11thLauncher
     public static class Constants
     {
         //
-        // Settings Service
-        //
-        public static readonly string[] Languages = { "en-US", "es-ES" };
-        public static readonly string ConfigPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "11th Launcher");
-        public const string ConfigFileName = "config.json";
-        public static readonly string[] Arma3RegPath32 = { @"HKEY_LOCAL_MACHINE\SOFTWARE\Bohemia Interactive\ArmA 3", "MAIN", null };
-        public static readonly string[] Arma3RegPath64 = { @"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Bohemia Interactive\ArmA 3", "MAIN", null };
-        public static readonly string[] SteamRegPath32 = { @"HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam", "InstallPath", "" };
-        public static readonly string[] SteamRegPath64 = { @"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam", "InstallPath", "" };
-        public const string DefaultArma3SteamPath = @"SteamApps\common\ArmA 3";
-        public const string GameExecutable32 = "arma3.exe";
-        public const string GameExecutable64 = "arma3_x64.exe";
-
-        //
         // Addon Service
         //
         public static readonly string[] VanillaAddons = { "arma 3", "expansion", "curator", "kart", "heli", "mark", "jets", "orange", "argo", "tacops", "tanks", "dlcbundle" };
         public const string AddonSubfolderName = "addons";
         public const string AddonFilePattern = "*.?bo";
         public const string AddonMetaDataFile = "mod.cpp";
-
-        //
-        // Updater Service
-        //
-        public const string VersionUrl = "http://raw.githubusercontent.com/11thmeu/launcher/master/bin/version";
-        public const string DownloadBaseUrl = "https://raw.githubusercontent.com/11thmeu/launcher/master/bin/";
-        public static readonly string UpdaterPath = Path.Combine(Path.GetTempPath(), "11thLauncherUpdater.exe");
-
-        //
-        // Server Status Service
-        //
-        public static readonly Server[] DefaultServers = 
-        {
-            new Server
-            {
-                Name = "Coop",
-                Address = "11thmeu.es",
-                Port = 2302,
-                IsDefault = true,
-                IsEnabled = true
-            },
-            new Server {
-                Name = "Academia",
-                Address = "11thmeu.es",
-                Port = 2322,
-                IsDefault = true,
-                IsEnabled = true
-            },
-            new Server {
-                Name = "ALiVE",
-                Address = "11thmeu.es",
-                Port = 2332,
-                IsDefault = true,
-                IsEnabled = true
-            },
-            new Server{
-                Name = "Test",
-                Address = "11thmeu.es",
-                Port = 2342,
-                IsDefault = true,
-                IsEnabled = true
-            },
-            new Server {
-                Name = "Vietnam",
-                Address = "11thmeu.es",
-                Port = 2352,
-                IsDefault = true,
-                IsEnabled = true
-            }
-        };
-
-
-        //
-        // Arma3Sync Service
-        //
-        public const string JavaExecutable = "java.exe";
-        public const string JavaPathCommand = "java";
-        public const string JavaRuntimeBinaryFolder = "bin";
-        public const string Arma3SyncBaseRegistryPath32 = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
-        public const string Arma3SyncBaseRegistryPath64 = @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall";
-        public const string Arma3SyncRegDisplayNameEntry = "DisplayName";
-        public const string Arma3SyncRegDisplayNameValue = "ArmA3Sync";
-        public const string Arma3SyncRegLocationEntry = "InstallLocation";
-        public const string Arma3SyncExecutable = "ArmA3Sync.exe";
-        public const string Arma3SyncConfigFolder = @"resources\ftp\";
-        public const string Arma3SyncRepositoryExtension = @".a3s.repository";
-        public const string Arma3SyncRemoteServerInfo = @"ftp://{0}/.a3s/serverinfo";
-
-        // 
-        // 11th MEU addon presets
-        //
         public static readonly BindableCollection<Preset> AddonPresets = new BindableCollection<Preset>
         {
             new Preset
@@ -120,9 +35,89 @@ namespace _11thLauncher
         };
 
         //
-        // Repository Settings
+        // Server Status Service
+        //
+        public static readonly Server[] DefaultServers = 
+        {
+            new Server
+            {
+                Name = "Coop",
+                Address = "11thmeu.es",
+                Port = 2302,
+                IsDefault = true,
+                IsEnabled = true
+            },
+            new Server
+            {
+                Name = "Academia",
+                Address = "11thmeu.es",
+                Port = 2322,
+                IsDefault = true,
+                IsEnabled = true
+            },
+            new Server
+            {
+                Name = "ALiVE",
+                Address = "11thmeu.es",
+                Port = 2332,
+                IsDefault = true,
+                IsEnabled = true
+            },
+            new Server
+            {
+                Name = "Test",
+                Address = "11thmeu.es",
+                Port = 2342,
+                IsDefault = true,
+                IsEnabled = true
+            },
+            new Server
+            {
+                Name = "Vietnam",
+                Address = "11thmeu.es",
+                Port = 2352,
+                IsDefault = true,
+                IsEnabled = true
+            }
+        };
+
+        //
+        // Arma3Sync Service
         //
         public static readonly string A3SdsPath = Path.Combine(Path.GetTempPath(), "A3SDS.jar");
+        public const string JavaExecutable = "java.exe";
+        public const string JavaPathCommand = "java";
+        public const string JavaRuntimeBinaryFolder = "bin";
+        public const string Arma3SyncBaseRegistryPath32 = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+        public const string Arma3SyncBaseRegistryPath64 = @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall";
+        public const string Arma3SyncRegDisplayNameEntry = "DisplayName";
+        public const string Arma3SyncRegDisplayNameValue = "ArmA3Sync";
+        public const string Arma3SyncRegLocationEntry = "InstallLocation";
+        public const string Arma3SyncExecutable = "ArmA3Sync.exe";
+        public const string Arma3SyncConfigFolder = @"resources\ftp\";
+        public const string Arma3SyncRepositoryExtension = @".a3s.repository";
+        public const string Arma3SyncRemoteServerInfo = @"ftp://{0}/.a3s/serverinfo";
+
+        //
+        // Updater Service
+        //
+        public const string VersionUrl = "http://raw.githubusercontent.com/11thmeu/launcher/master/bin/version";
+        public const string DownloadBaseUrl = "https://raw.githubusercontent.com/11thmeu/launcher/master/bin/";
+        public static readonly string UpdaterPath = Path.Combine(Path.GetTempPath(), "11thLauncherUpdater.exe");
+
+        //
+        // Settings Service
+        //
+        public static readonly string[] Languages = { "en-US", "es-ES" };
+        public static readonly string ConfigPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "11th Launcher");
+        public const string ConfigFileName = "config.json";
+        public static readonly string[] Arma3RegPath32 = { @"HKEY_LOCAL_MACHINE\SOFTWARE\Bohemia Interactive\ArmA 3", "MAIN", null };
+        public static readonly string[] Arma3RegPath64 = { @"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Bohemia Interactive\ArmA 3", "MAIN", null };
+        public static readonly string[] SteamRegPath32 = { @"HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam", "InstallPath", "" };
+        public static readonly string[] SteamRegPath64 = { @"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam", "InstallPath", "" };
+        public const string DefaultArma3SteamPath = @"SteamApps\common\ArmA 3";
+        public const string GameExecutable32 = "arma3.exe";
+        public const string GameExecutable64 = "arma3_x64.exe";
 
         //
         // Program Settings
